66e1fef7c1872f31907cc207fcd02093e99942a7
giandonato.inverso@edempg.it <giandonato.inverso@studenti.unimi.it>
2024-07-15 01:22:21 +0200

    sshpass

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo docker build -t raspberry_qemu_image .
 sudo docker run -d --name qemu_container -p 2225:2225 raspberry_qemu_image
 
 check_ssh_connection() {
-    ssh -p 2225 pi@localhost exit
+    sshpass -p raspberry ssh -p 2225 pi@localhost exit
 }
 
 until check_ssh_connection; do
