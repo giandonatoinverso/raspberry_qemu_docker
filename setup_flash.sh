8207b4a5f1d40f138001f90664fdf3d7715d9433
giandonato.inverso@edempg.it <giandonato.inverso@studenti.unimi.it>
2024-07-15 02:04:35 +0200

    edit setupflash

--- setup_flash.sh
+++ setup_flash.sh
@@ -18,8 +18,8 @@ OFFSET=$((START_SECTOR * 512))
 
 sudo mount -o loop,offset=$OFFSET $IMG_FILE /mnt
 sudo sed -i 's/^/# /' /mnt/etc/ld.so.preload
-sudo echo "raspberryPi:$6$rAnjgjG0.K8cPrnU$6Wk1GT0zwFc9CFQrEZi8NMxDtWKzsQN0x/p.x0/tJFYN9FJjphX9n2pXNMFvBDhYxMF1BJUTdS7sRO987dv6S1:1001:0:raspberryPi:/root:/bin/bash" >> /mnt/etc/passwd
-sudo echo "raspberryPi	ALL=(ALL:ALL) NOPASSWD: ALL" >> /mnt/etc/sudoers
+sudo echo "pi:$6$rBoByrWRKMY1EHFy$ho.LISnfm83CLBWBE/yqJ6Lq1TinRlxw/ImMTPcvvMuUfhQYcMmFnpFXUPowjy2br1NA0IACwF9JKugSNuHoe0:1001:0:raspberryPi:/root:/bin/bash" >> /mnt/etc/passwd
+sudo echo "pi	ALL=(ALL:ALL) NOPASSWD: ALL" >> /mnt/etc/sudoers
 sudo sed -i 's/defaults,noatime,ro/defaults,noatime,rw/' /mnt/etc/fstab
 sudo rm /mnt/etc/ssh/sshd_config
 sudo cp config/sshd_config /mnt/etc/ssh/sshd_config
@@ -33,7 +33,7 @@ sudo docker build -t raspberry_qemu_image .
 sudo docker run -d --name qemu_container -p 2225:2225 raspberry_qemu_image
 
 check_ssh_connection() {
-    ssh -i config/id_rsa -o PreferredAuthentications=publickey -o StrictHostKeyChecking=no -o ConnectTimeout=60 -q -p 2225 lynis@localhost exit
+    ssh -i config/id_rsa -o PreferredAuthentications=publickey -o StrictHostKeyChecking=no -o ConnectTimeout=60 -q -p 2225 pi@localhost exit
 }
 
 until check_ssh_connection; do
